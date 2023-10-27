d834be3aa3d534017fb6bed77edf1c87a7d5177c
Nimlos <emil@emilos.dk>
2023-10-27 22:35:17 +0200

    Init

---
+++ src/test/java/com/enr/carfueltracker/CarfueltrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enr.carfueltracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarfueltrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
